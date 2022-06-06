5580f68454192bf556bb0ea7d9abe1e71d9436a2
r3cro <logan.manlief@gmail.com>
2022-06-06 19:39:19 -0400

    removed unused imports, added returns

--- src/main/java/me/recro/bedwars/commands/DevCommand.java
+++ src/main/java/me/recro/bedwars/commands/DevCommand.java
@@ -3,8 +3,10 @@ package me.recro.bedwars.commands;
 import lombok.AllArgsConstructor;
 import me.recro.bedwars.BedWars;
 import me.recro.bedwars.core.GameArena;
+import me.recro.bedwars.core.constant.GameState;
 import me.recro.bedwars.core.constant.Messages;
 import me.recro.bedwars.core.constant.menu.ShopMenu;
+import me.recro.bedwars.utils.Utils;
 import me.recro.bedwars.utils.execeptions.MenuManagerException;
 import me.recro.bedwars.utils.execeptions.MenuManagerNotSetupException;
 import me.recro.bedwars.utils.menus.MenuManager;
@@ -31,7 +33,7 @@ public class DevCommand implements CommandExecutor {
             player.sendMessage("/dev start");
             player.sendMessage("/dev end");
             player.sendMessage("/dev menu");
-            Messages.GAME_NOT_RUNNING.send(player);
+            player.sendMessage(Utils.color("&eGameState: &c" + gameArena.getGameState()));
             return true;
         }
         if(args[0].equalsIgnoreCase("start")) {
@@ -39,20 +41,24 @@ public class DevCommand implements CommandExecutor {
                 gameArena.MINIMUM_PLAYERS = 0;
                 gameArena.startCountdown();
                 Messages.GAME_STARTING.send(player);
+                return true;
             }
         }
         if(args[0].equalsIgnoreCase("end")) {
             if(!gameArena.isGameRunning()) {
                 Messages.GAME_NOT_RUNNING.send(player);
+                return true;
             }
             gameArena.endGame();
             gameArena.MINIMUM_PLAYERS = 4;
             Messages.GAME_OVER.send(player);
+            return true;
         }
 
         if(args[0].equalsIgnoreCase("menu")) {
             try {
                 MenuManager.openMenu(ShopMenu.class, player);
+                return true;
             } catch (MenuManagerException | MenuManagerNotSetupException e) {
                 throw new RuntimeException(e);
             }
